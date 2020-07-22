843925540df83f5ed5dc8a64c2f8b529342efd2c
Kamil Bakierzynski <bakierzynskikamil@gmail.com>
2020-07-22 17:00:28 +0200

    Change the way of calculating "brightness", better README.md, config
    * Change from relative luminance to HSV
    * Description about the solution in README.md
    * Added config file from where it reads paths and threshold

--- README.md
+++ README.md
@@ -1,1 +1,40 @@
-"# recruitment-assignement" 
+# Are you afraid of the dark?
+
+### Used tools
+I've chosen to write a solution using Scala with Akka Actors.
+It allows me to use parallelism needed to achieve good results even
+when the system is on heavy load. System is creating new actor for every photo
+and every worker is responsible of opening, calculating and saving image.
+There is one actor called boss - responsible of providing tasks for workers.
+
+### Configuration
+In the `application.conf` user can specify path to samples and path to output.
+In the same spot you can adjust threshold value for "cut off" point.
+To the images that were provided with the task I added all black and all white to test
+if the algorithm gives them respectively 100 and 0 points. There are also couple
+images from the web I have downloaded from the internet.
+
+### Summary
+All the images provided with the task are classified correctly
+however I have specified "cut off" point at around 70. It's high but
+most of the images in the dark folder with samples get scores around 98 (lowest score 84) so the
+70 should do the work. It's easily accessible so it's no problem to tweak on the go.
+
+### Solution description
+To calculate the "brightness" i use the V (value) from the HSV color scheme.
+It simple to calculate - max value of red, green, blue divided by 255. After that
+I get the average of previously calculated value of every pixel in the image. The result
+is presented as percentage from 0 (black) to 100 (white). I take the opposite of that
+to get the desired score.
+
+# Score analysis
+>> All images that were provided are correctly classified (14 in each class + all white and all black for 0 and 100 score respectively)
+
+| Actual/Result | Bright | Dark |
+|---------------|--------|------|
+| Bright        | 15     | 0    |
+| Dark          | 0      | 15   |
+
+
+
+
--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -5,11 +5,13 @@ import javax.imageio.ImageIO
 import java.awt.image.BufferedImage
 import java.awt.Color
 
+import com.typesafe.config.ConfigFactory
+
 // Case classes for messaging
 // Message for boss with the path of the image to check
 case class Path(path: String)
 // Message for worker with the read image + specified settings
-case class Photo(photo: BufferedImage, threshold: Int, outPath: String)
+case class Photo(path: String, threshold: Int, outPath: String)
 // Initial message to boss with program settings
 case class Settings(threshold: Int, outPath: String)
 // Message to boss when the file is saved properly
@@ -32,18 +34,12 @@ class Boss extends Actor {
     // Receives path of an image to check
     case Path(path) =>
       // Opens image from the provided path
-      try {
-        val photo = ImageIO.read(new File(path))
-        // Gets the name of the file to name worker properly
-        val name = path.split("\\\\").last
-        val worker = context.actorOf(Props[Worker], name = name)
-        // Increases counter of photos to process
-        photosToProcess += 1
-        worker ! Photo(photo, threshold, outPath)
-      } catch {
-        case exception: IOException => println(s"IOException:\n $exception")
-        case exception: Exception => println(s"UndefinedException\n $exception")
-      }
+      // Gets the name of the file to name worker properly
+      val name = path.split("\\\\").last
+      val worker = context.actorOf(Props[Worker], name = name)
+      // Increases counter of photos to process
+      photosToProcess += 1
+      worker ! Photo(path, threshold, outPath)
 
     // Notify user when the worker finished his job
     case Finish(filePath) =>
@@ -58,21 +54,22 @@ class Boss extends Actor {
 // Worker class used to calculate image luminance values and save pictures to provided folder
 class Worker extends Actor {
   def receive: Receive = {
-    case Photo(photo, threshold, outPath) =>
+    case Photo(path, threshold, outPath) =>
+      val photo = ImageIO.read(new File(path))
       /* Calculates what is the max value of the all white photo. I uses it for reference to calculate
       *  percentage of "whiteness" */
-      val maxWhite = calculateMaxWhiteValue(photo)
+      val pixelCount = photo.getWidth * photo.getHeight
 
-      // Calculates picture "whiteness" value using Relative luminance formula (ITU-R BT.709)
+      // Gets average (in percentage) of Value from RGB to HSV conversion
       val sum = calculatePixels(photo)
 
       /* Switches the scale to all black
       *  calculates the percentage of the "whiteness" of the image
       *  subtracts it from 1.0 to get the "darkness */
-      val darkVal = 1.0 - (sum * 1.0 / maxWhite)
+      val darkVal = 100 - (sum * 1.0 / pixelCount)
 
       // Rounds result to get two digits
-      val result = BigDecimal(darkVal).setScale(2, BigDecimal.RoundingMode.HALF_UP).toDouble
+      val result = Math.round(darkVal).toInt
 
       /* Gets format of the original file + the path and name to save it
       *  namesToSave is a tuple containing on ._1 - PATH to save image and on ._2 format of original */
@@ -85,7 +82,7 @@ class Worker extends Actor {
         case exception: IOException => println(s"IOException:\n $exception")
         case exception: Exception => println(s"UndefinedException:\n $exception")
       }
-      // Declares finished job to the boss
+      // Declares finished job to the boss, sends path to display in console
       sender() ! Finish(namesToSave._1)
 
       // Stops working
@@ -110,21 +107,16 @@ class Worker extends Actor {
           val green = pixelColor.getGreen
           val blue = pixelColor.getBlue
 
-          // Relative luminance formula
-          val luminance = 0.2126 * red + 0.7152 * green + 0.0722 * blue
+          // "Converting" to HSV to obtain V (value)
+          val valueFromHSV = Math.max(red/255.0, Math.max(green/255.0, blue/255.0)) * 100
           // Sum accumulator with calculated value from formula and go to the next pixel in column
-          helper(accumulator + luminance, calcWidth, calcHeight + 1)
+          helper(accumulator + valueFromHSV, calcWidth, calcHeight + 1)
         }
     }
-    helper(0, 0, 0)
-  }
-  // Function to calculate value for all white image of the original size
-  def calculateMaxWhiteValue(photo: BufferedImage): Double = {
-    val whiteColorVal = 255 // Value of a white pixel (255, 255, 255) in RGB
-    photo.getWidth * photo.getHeight * (0.2126 * whiteColorVal + 0.7152 * whiteColorVal + 0.0722 * whiteColorVal)
+    helper(0.0, 0, 0)
   }
   // Function to prepare name of the image to save
-  def prepareName(workerName: String, threshold: Int, result: Double, outPath: String): (String, String) = {
+  def prepareName(workerName: String, threshold: Int, result: Int, outPath: String): (String, String) = {
 
     // Name of the original file without extension
     val nameWithoutExtension = workerName.split('.').head
@@ -133,13 +125,10 @@ class Worker extends Actor {
     val format = workerName.split('.').last
 
     // Prepare dark or bright metadata
-    val resultString = if (result * 100 > threshold) "dark" else "bright"
-
-    // Convert from double to int 0.57 -> 57
-    val formatResult = (result * 100).toString.split('.').head
+    val resultString = if (result > threshold) "dark" else "bright"
 
     // Return tuple containing (path, format)
-    (outPath + "/" + nameWithoutExtension + "_" + resultString + "_" + formatResult + "." + format, format)
+    (outPath + "/" + nameWithoutExtension + "_" + resultString + "_" + result.toString + "." + format, format)
   }
 }
 
@@ -151,10 +140,13 @@ object Main extends App {
   // Create boss actor (head of the system)
   val boss = system.actorOf(Props[Boss], name="boss")
 
+  // Read config file
+  val configFile = ConfigFactory.parseFile(new File("application.conf"))
+
   // Settings
-  val THRESHOLD = 75 // Value used to define the bright/dark barrier
-  val outDir = "src/main/out" // Directory to save checked pictures
-  val sourceDir = "src/main/in" // Directory from where the program gets samples
+  val THRESHOLD = configFile.getInt("settings.variables.threshold")// Value used to define the bright/dark barrier
+  val outDir = configFile.getString("settings.paths.output") // Directory to save pictures
+  val sourceDir = configFile.getString("settings.paths.input") // Directory from where the program gets samples
 
   // Apply settings from above to boss actor
   boss ! Settings(THRESHOLD, outDir)
